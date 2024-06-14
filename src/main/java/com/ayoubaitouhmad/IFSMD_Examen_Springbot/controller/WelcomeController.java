cca78bb3be64a754e22286b7944df22ea2324a04
mrx <ayoubaithmad77@gmail.com>
2024-06-14 16:01:53 +0100

    wip

--- src/main/java/com/ayoubaitouhmad/IFSMD_Examen_Springbot/controller/WelcomeController.java
+++ src/main/java/com/ayoubaitouhmad/IFSMD_Examen_Springbot/controller/WelcomeController.java
@@ -1,16 +1,63 @@
 package com.ayoubaitouhmad.IFSMD_Examen_Springbot.controller;
 
+import com.ayoubaitouhmad.IFSMD_Examen_Springbot.config.DbConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Map;
+
+@Slf4j
 @Controller
+@RestController
 public class WelcomeController {
+    private static final Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+
+    @Autowired
+    private DataSource dataSource;
+
 
     @GetMapping("/welcome")
-    public ModelAndView welcome(ModelAndView modelAndView) {
+    public ModelAndView welcome(ModelAndView modelAndView) throws SQLException {
         modelAndView.setViewName("index");  // The name of the Thymeleaf template
         modelAndView.addObject("message", "Hello, welcome to our website!");
         return modelAndView;
     }
+
+    @GetMapping("/login")
+    public ModelAndView login(ModelAndView modelAndView) throws SQLException {
+        modelAndView.setViewName("login");  // The name of the Thymeleaf template
+        modelAndView.addObject("message", "Hello, welcome to our website!");
+        return modelAndView;
+    }
+
+    @PostMapping("/registre")
+    public ModelAndView nice(ModelAndView modelAndView, @RequestParam Map<String, String> formData) {
+        logger.info(formData.get("name"));
+        modelAndView.setViewName("index");  // The name of the Thymeleaf template
+        modelAndView.addObject("message", "Hello, welcome to our website!");
+        return modelAndView;
+    }
+
+
+    @GetMapping("/database")
+
+    public String database() {
+        try (Connection connection = dataSource.getConnection()) {
+            return "Database connected successfully.";
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return "Database connection failed.";
+        }
+    }
+
+
 }
