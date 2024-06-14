01ec4c0ca5a69858903564c43b7b0f20f0d270f8
mrx <ayoubaithmad77@gmail.com>
2024-06-14 16:01:17 +0100

    configure DataSourceConfig

---
+++ src/main/java/com/ayoubaitouhmad/IFSMD_Examen_Springbot/config/DbConfig.java
@@ -0,0 +1,23 @@
+package com.ayoubaitouhmad.IFSMD_Examen_Springbot.config;
+
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DbConfig {
+
+    @Bean
+    public static DataSource dataSource() {
+        return DataSourceBuilder.create()
+                .url("jdbc:mysql://localhost:3306/ifsmd_examen_springbot")
+                .username("root")
+                .password("")
+                .driverClassName("com.mysql.cj.jdbc.Driver")
+                .build();
+    }
+}
+
+
