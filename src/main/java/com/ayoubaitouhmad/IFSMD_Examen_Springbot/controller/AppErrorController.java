dbb722ed529330841a4a8cc4817cd3e9531030ef
mrx <ayoubaithmad77@gmail.com>
2024-06-14 16:03:33 +0100

    wip

---
+++ src/main/java/com/ayoubaitouhmad/IFSMD_Examen_Springbot/controller/AppErrorController.java
@@ -0,0 +1,45 @@
+package com.ayoubaitouhmad.IFSMD_Examen_Springbot.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.boot.web.error.ErrorAttributeOptions;
+import org.springframework.boot.web.servlet.error.ErrorAttributes;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.ServletWebRequest;
+
+import java.util.Map;
+
+@Controller
+public class AppErrorController implements ErrorController {
+    private final ErrorAttributes errorAttributes;
+
+    public AppErrorController(ErrorAttributes errorAttributes) {
+        this.errorAttributes = errorAttributes;
+    }
+
+    @RequestMapping("/error")
+    public ResponseEntity<Map<String, Object>> handleError(HttpServletRequest request) {
+        ServletWebRequest servletWebRequest = new ServletWebRequest(request);
+        Map<String, Object> errorAttributesMap = errorAttributes.getErrorAttributes(servletWebRequest, ErrorAttributeOptions.defaults());
+        HttpStatus status = getStatus(request);
+        return new ResponseEntity<>(errorAttributesMap, status);
+    }
+
+    private HttpStatus getStatus(HttpServletRequest request) {
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        if (statusCode == null) {
+            return HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        return HttpStatus.valueOf(statusCode);
+    }
+
+
+
+}
