5996dc63a80ae78bd2ca55714d99c6e93b2c1d5a
mrx <ayoubaithmad77@gmail.com>
2024-06-16 16:25:51 +0100

    refactor: use template design instead of static design

--- src/main/java/com/ayoubaitouhmad/IFSMD_Examen_Springbot/controller/auth/LoginController.java
+++ src/main/java/com/ayoubaitouhmad/IFSMD_Examen_Springbot/controller/auth/LoginController.java
@@ -1,8 +1,6 @@
 package com.ayoubaitouhmad.IFSMD_Examen_Springbot.controller.auth;
 
 import com.ayoubaitouhmad.IFSMD_Examen_Springbot.controller.BaseController;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -17,7 +15,7 @@ public class LoginController extends BaseController {
         if (isConnected()) {
             return "redirect:/welcome";
         } else {
-            return "login";
+            return "pages/auth/login";
         }
     }
 
