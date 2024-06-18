4d020323ca0c3396c94f5474ed5233a4e6a32fb3
mrx <ayoubaithmad77@gmail.com>
2024-06-18 17:20:22 +0100

    add timestamps

--- src/main/java/com/ayoubaitouhmad/IFSMD_Examen_Springbot/model/User.java
+++ src/main/java/com/ayoubaitouhmad/IFSMD_Examen_Springbot/model/User.java
@@ -1,73 +1,111 @@
 package com.ayoubaitouhmad.IFSMD_Examen_Springbot.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
-@Table(name = "users")
-public class User {
+@Table(name = "users" , schema = "ifsmd_examen_springbot")
+public class User  implements UserDetails {
 
+
+    @Getter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+
+    @Setter
+    @Getter
     private String name;
+
+    @Setter
+    @Getter
     private String email;
+
+    @Setter
+    @Getter
     private String password;
+
+    @Getter
+    @Setter
     private String username;
+
+
+    @Setter
+    @Getter
     private String role;
 
-    public String getRole() {
-        return role;
-    }
 
-    public void setRole(String role) {
-        this.role = role;
-    }
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "profile_image_id", referencedColumnName = "id")
+    private FileDocument profileImage;
 
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
+    public FileDocument getProfileImage() {
+        return this.profileImage;
     }
 
 
-    public Long getId() {
-        return id;
+
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    @Getter
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Getter
+    private LocalDateTime updatedAt;
+
+
+
+
+
+
+
+
+    public User() {
     }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
 
     @Override
     public String toString() {
         return "name=" + name + ", email=" + email + ", password=" + password + ", username=" + username + ", id=" + id;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
     }
 }
