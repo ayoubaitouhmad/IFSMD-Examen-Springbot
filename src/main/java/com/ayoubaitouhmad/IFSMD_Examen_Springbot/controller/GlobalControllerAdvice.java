3c0b1be4d6fa5883781b7deca3803ebadd617d7f
mrx <ayoubaithmad77@gmail.com>
2024-06-18 17:52:06 +0100

    get user directly from user details

--- src/main/java/com/ayoubaitouhmad/IFSMD_Examen_Springbot/controller/GlobalControllerAdvice.java
+++ src/main/java/com/ayoubaitouhmad/IFSMD_Examen_Springbot/controller/GlobalControllerAdvice.java
@@ -1,20 +1,33 @@
 package com.ayoubaitouhmad.IFSMD_Examen_Springbot.controller;
 
+import com.ayoubaitouhmad.IFSMD_Examen_Springbot.model.User;
+import com.ayoubaitouhmad.IFSMD_Examen_Springbot.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
 
+
+
+    private final UserService userService;
+
+    @Autowired
+    public GlobalControllerAdvice(UserService userService) {
+        this.userService = userService;
+    }
+
     @ModelAttribute("currentConnectedUser")
-    public UserDetails currentUser() {
+    public Optional<User> getCurrentConnectedUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if (authentication != null && authentication.isAuthenticated() && authentication.getPrincipal() instanceof UserDetails) {
-            return (UserDetails) authentication.getPrincipal();
+        if (authentication != null && authentication.isAuthenticated() && !authentication.getName().equals("anonymousUser")) {
+            return userService.findByUserName(authentication.getName());
         }
-        return null; // return null or a default UserDetails implementation for unauthenticated access
+        return Optional.empty();
     }
 }
