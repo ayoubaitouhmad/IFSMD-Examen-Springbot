7df7cc32f32f3b659941081ed096e61a200a6572
mrx <ayoubaithmad77@gmail.com>
2024-06-21 19:50:59 +0100

    add helper func to get current connected user

--- src/main/java/com/ayoubaitouhmad/IFSMD_Examen_Springbot/controller/BaseController.java
+++ src/main/java/com/ayoubaitouhmad/IFSMD_Examen_Springbot/controller/BaseController.java
@@ -1,7 +1,10 @@
 package com.ayoubaitouhmad.IFSMD_Examen_Springbot.controller;
 
+import com.ayoubaitouhmad.IFSMD_Examen_Springbot.model.User;
+import com.ayoubaitouhmad.IFSMD_Examen_Springbot.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -13,8 +16,13 @@ public class BaseController {
      * debug
      */
     protected static final Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+    @Autowired UserService userService;
 
 
-
+    public User getCurrentUser() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String currentUsername = authentication.getName();
+        return userService.findByUserName(currentUsername).orElseThrow(() -> new RuntimeException("User not found"));
+    }
 
 }
