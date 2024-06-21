67dfd4f82de8541e3becf56cee4267b905ff0241
mrx <ayoubaithmad77@gmail.com>
2024-06-21 19:55:10 +0100

    home page for all articles

--- src/main/java/com/ayoubaitouhmad/IFSMD_Examen_Springbot/controller/user/DashboardController.java
+++ src/main/java/com/ayoubaitouhmad/IFSMD_Examen_Springbot/controller/user/DashboardController.java
@@ -2,16 +2,31 @@ package com.ayoubaitouhmad.IFSMD_Examen_Springbot.controller.user;
 
 
 import com.ayoubaitouhmad.IFSMD_Examen_Springbot.controller.BaseController;
+import com.ayoubaitouhmad.IFSMD_Examen_Springbot.model.Article;
+import com.ayoubaitouhmad.IFSMD_Examen_Springbot.model.User;
+import com.ayoubaitouhmad.IFSMD_Examen_Springbot.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+
 @Controller
-public class DashboardController  extends BaseController {
+public class DashboardController extends BaseController {
+    private  final ArticleService articleService;
+
+    @Autowired
+    public DashboardController(ArticleService articleService) {
+        this.articleService = articleService;
+    }
+
 
     @GetMapping("/home")
-    public String welcome(Model model ) {
-        model.addAttribute("pageTitle" , "Home");  // The name of the Thymeleaf template
+    public String welcome(Model model) {
+        model.addAttribute("pageTitle", "Home");
+        List<Article> articles = articleService.getAllArticles();
+        model.addAttribute("articles", articles);
         return "pages/user/home";
     }
 
