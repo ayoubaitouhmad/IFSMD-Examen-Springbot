88fe8c35351336634537b04cb570bdb59f75c2c4
mrx <ayoubaithmad77@gmail.com>
2024-06-23 09:45:30 +0100

    clean up

--- src/main/java/com/ayoubaitouhmad/IFSMD_Examen_Springbot/controller/user/DashboardController.java
+++ src/main/java/com/ayoubaitouhmad/IFSMD_Examen_Springbot/controller/user/DashboardController.java
@@ -1,14 +1,12 @@
 package com.ayoubaitouhmad.IFSMD_Examen_Springbot.controller.user;
 
-
 import com.ayoubaitouhmad.IFSMD_Examen_Springbot.controller.BaseController;
 import com.ayoubaitouhmad.IFSMD_Examen_Springbot.model.Article;
-import com.ayoubaitouhmad.IFSMD_Examen_Springbot.model.User;
+import com.ayoubaitouhmad.IFSMD_Examen_Springbot.model.BaseModel;
 import com.ayoubaitouhmad.IFSMD_Examen_Springbot.service.ArticleService;
+import com.ayoubaitouhmad.IFSMD_Examen_Springbot.util.PageUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/home")
@@ -28,27 +25,14 @@ public class DashboardController extends BaseController {
         this.articleService = articleService;
     }
 
-
     @GetMapping("")
-    public String index(Model model, @RequestParam(value = "search" , defaultValue = "") String searchTerm, @RequestParam(value = "page", defaultValue = "0") int page) {
+    public String index(Model model, @RequestParam(value = "search", defaultValue = "") String searchTerm, @RequestParam(value = "page", defaultValue = "0") int page) {
         model.addAttribute("pageTitle", "Home Page");
-        Page<Article> articlePage = articleService.getAllArticles(page);
+        List<Article> articles = articleService.getAllArticles();
+        articles = articleService.filterArticlesListByTitleAndContent(searchTerm, searchTerm, articles);
+        Page<BaseModel> articlePage = PageUtil.PageList(articles, page);
+        model.addAttribute("searchTerm", searchTerm);
         model.addAttribute("articles", articlePage);
-
-        if (searchTerm != null && !searchTerm.isEmpty()) {
-            List<Article> filteredArticles = articlePage.stream()
-                    .filter(article -> article.getTitle().toLowerCase().contains(searchTerm.toLowerCase()) ||
-                            article.getContent().toLowerCase().contains(searchTerm.toLowerCase()))
-                    .collect(Collectors.toList());
-            articlePage = new PageImpl<>(filteredArticles, PageRequest.of(page, 8), filteredArticles.size());
-            model.addAttribute("searchTerm", searchTerm);
-            model.addAttribute("articles", articlePage);
-
-        }
-
         return "pages/user/home";
-
     }
-
-
 }
