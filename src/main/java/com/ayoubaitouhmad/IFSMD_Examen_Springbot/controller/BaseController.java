b54a7f34a054f2bc66098631783955a5a523b50f
mrx <ayoubaithmad77@gmail.com>
2024-06-23 08:59:34 +0100

    integrate http errors in all controller

--- src/main/java/com/ayoubaitouhmad/IFSMD_Examen_Springbot/controller/BaseController.java
+++ src/main/java/com/ayoubaitouhmad/IFSMD_Examen_Springbot/controller/BaseController.java
@@ -5,10 +5,10 @@ import com.ayoubaitouhmad.IFSMD_Examen_Springbot.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Controller;
-
+import org.springframework.web.server.ResponseStatusException;
 
 public class BaseController {
 
@@ -16,13 +16,24 @@ public class BaseController {
      * debug
      */
     protected static final Logger logger = LoggerFactory.getLogger(WelcomeController.class);
-    @Autowired UserService userService;
-
+    @Autowired
+    UserService userService;
 
     public User getCurrentUser() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         String currentUsername = authentication.getName();
         return userService.findByUserName(currentUsername).orElseThrow(() -> new RuntimeException("User not found"));
+    }
+
+    public void http404() {
+        throw new ResponseStatusException(
+                HttpStatus.NOT_FOUND, ""
+        );
+    }
+    public void http403() {
+        throw new ResponseStatusException(
+                HttpStatus.FORBIDDEN, ""
+        );
     }
 
 }
